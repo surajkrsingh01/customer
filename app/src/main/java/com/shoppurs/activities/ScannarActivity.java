54fcf0382c3a6d18733c4e32a0b96d2759f5bb38
suraj_essentrix <suraj@essentrix.in>
2019-12-06 17:09:51 +0530

    Commited by Suraj

--- app/src/main/java/com/shoppurs/activities/ScannarActivity.java
+++ app/src/main/java/com/shoppurs/activities/ScannarActivity.java
@@ -21,20 +21,6 @@ import com.journeyapps.barcodescanner.BarcodeCallback;
 import com.journeyapps.barcodescanner.BarcodeResult;
 import com.journeyapps.barcodescanner.CaptureManager;
 import com.journeyapps.barcodescanner.DecoratedBarcodeView;
-import com.scandit.datacapture.barcode.capture.BarcodeCapture;
-import com.scandit.datacapture.barcode.capture.BarcodeCaptureListener;
-import com.scandit.datacapture.barcode.capture.BarcodeCaptureSession;
-import com.scandit.datacapture.barcode.capture.BarcodeCaptureSettings;
-import com.scandit.datacapture.barcode.capture.SymbologySettings;
-import com.scandit.datacapture.barcode.data.Symbology;
-import com.scandit.datacapture.barcode.data.SymbologyDescription;
-import com.scandit.datacapture.core.capture.DataCaptureContext;
-import com.scandit.datacapture.core.common.feedback.Feedback;
-import com.scandit.datacapture.core.data.FrameData;
-import com.scandit.datacapture.core.source.Camera;
-import com.scandit.datacapture.core.source.CameraSettings;
-import com.scandit.datacapture.core.source.FrameSourceState;
-import com.scandit.datacapture.core.ui.DataCaptureView;
 import com.shoppurs.R;
 import com.shoppurs.models.Barcode;
 import com.shoppurs.models.MyProduct;
@@ -60,13 +46,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
-public class ScannarActivity extends NetworkBaseActivity implements BarcodeCaptureListener {
+public class ScannarActivity extends NetworkBaseActivity {
 
-    //private CaptureManager capture;
-    //private DecoratedBarcodeView barcodeScannerView;
-    private BarcodeCapture barcodeCapture;
-    private DataCaptureContext dataCaptureContext;
-    private Camera camera;
+    private CaptureManager capture;
+    private DecoratedBarcodeView barcodeScannerView;
     private boolean isProduct;
     private String flag,type, shopCode, qrCode;
     private TextView text_header;
@@ -97,93 +80,63 @@ public class ScannarActivity extends NetworkBaseActivity implements BarcodeCaptu
             text_header.setText("Scan QR code of products");
         }
 
+        barcodeScannerView = findViewById(R.id.zxing_barcode_scanner);
 
+        //start capture
+        capture = new CaptureManager(this, barcodeScannerView);
+        capture.initializeFromIntent(getIntent(), savedInstanceState);
+        capture.decode();
+        //((ViewGroup) findViewById(R.id.scanner_container)).addView(dataCaptureView);
+    }
 
-        dataCaptureContext = DataCaptureContext.forLicenseKey(getResources().getString(R.string.scandit_license_key));
-        CameraSettings cameraSettings = BarcodeCapture.createRecommendedCameraSettings();
+    private void resetBarCodeScanner(){
+        barcodeScannerView.decodeSingle(new BarcodeCallback() {
+            @Override
+            public void barcodeResult(BarcodeResult result) {
+                Log.i(TAG,"result "+result.toString());
+                processResult(result.toString());
+            }
 
-// Depending on the use case further camera settings adjustments can be made here.
+            @Override
+            public void possibleResultPoints(List<ResultPoint> resultPoints) {
 
-        camera = Camera.getDefaultCamera();
+            }
+        });
+    }
 
-        if (camera != null) {
-            camera.applySettings(cameraSettings);
-        }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(capture != null)
+            capture.onPause();
+    }
 
-        if (camera != null) {
-            camera.switchToDesiredState(FrameSourceState.ON);
-        }
-
-        dataCaptureContext.setFrameSource(camera);
-
-        // The barcode capturing process is configured through barcode capture settings
-        // which are then applied to the barcode capture instance that manages barcode recognition.
-        BarcodeCaptureSettings barcodeCaptureSettings = new BarcodeCaptureSettings();
-
-        // The settings instance initially has all types of barcodes (symbologies) disabled.
-        // For the purpose of this sample we enable a very generous set of symbologies.
-        // In your own app ensure that you only enable the symbologies that your app requires as
-        // every additional enabled symbology has an impact on processing times.
-        HashSet<Symbology> symbologies = new HashSet<>();
-        symbologies.add(Symbology.EAN13_UPCA);
-        symbologies.add(Symbology.EAN8);
-        symbologies.add(Symbology.UPCE);
-        symbologies.add(Symbology.QR);
-        symbologies.add(Symbology.MICRO_QR);
-        symbologies.add(Symbology.DATA_MATRIX);
-        symbologies.add(Symbology.CODE39);
-        symbologies.add(Symbology.CODE128);
-        barcodeCaptureSettings.enableSymbologies(symbologies);
-
-        // Some linear/1d barcode symbologies allow you to encode variable-length data.
-        // By default, the Scandit Data Capture SDK only scans barcodes in a certain length range.
-        // If your application requires scanning of one of these symbologies, and the length is
-        // falling outside the default range, you may need to adjust the "active symbol counts"
-        // for this symbology. This is shown in the following few lines of code for one of the
-        // variable-length symbologies.
-        SymbologySettings symbologySettings =
-                barcodeCaptureSettings.getSymbologySettings(Symbology.CODE39);
-        SymbologySettings symbologySettingsqR =
-                barcodeCaptureSettings.getSymbologySettings(Symbology.QR);
-
-
-        HashSet<Short> activeSymbolCounts = new HashSet<>(
-                Arrays.asList(new Short[] { 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 }));
-
-        symbologySettings.setActiveSymbolCounts(activeSymbolCounts);
-        symbologySettingsqR.setActiveSymbolCounts(activeSymbolCounts);
-
-        symbologySettings.setColorInvertedEnabled(true);
-        symbologySettingsqR.setColorInvertedEnabled(true);
-
-        barcodeCapture = BarcodeCapture.forDataCaptureContext(dataCaptureContext, barcodeCaptureSettings);
-        barcodeCapture.addListener(this);
-
-        barcodeCapture.getFeedback().setSuccess(new Feedback(null,null));
-
-        DataCaptureView dataCaptureView = DataCaptureView.newInstance(this, dataCaptureContext);
-        // Add a barcode capture overlay to the data capture view to render the location of captured
-        // barcodes on top of the video preview.
-        // This is optional, but recommended for better visual feedback.
-        //BarcodeCaptureOverlay overlay = BarcodeCaptureOverlay.newInstance(barcodeCapture, dataCaptureView);
-        //  overlay.setViewfinder(new RectangularViewfinder());
-
-        ((ViewGroup) findViewById(R.id.scanner_container)).addView(dataCaptureView);
-
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(capture != null)
+            capture.onDestroy();
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        if(Utility.verifyCameraPermissions(this)){
-            barcodeCapture.setEnabled(true);
-            camera.switchToDesiredState(FrameSourceState.ON, null);
-        }
+        if(capture != null){
+            barcodeScannerView.decodeSingle(new BarcodeCallback() {
+                @Override
+                public void barcodeResult(BarcodeResult result) {
+                    Log.i(TAG,"result "+result.toString());
+                    processResult(result.toString());
+                }
 
-        // Check for camera permission and request it, if it hasn't yet been granted.
-        // Once we have the permission the onCameraPermissionGranted() method will be called.
-        // requestCameraPermission();
+                @Override
+                public void possibleResultPoints(List<ResultPoint> resultPoints) {
+
+                }
+            });
+
+            capture.onResume();
+        }
     }
 
 
@@ -222,27 +175,6 @@ public class ScannarActivity extends NetworkBaseActivity implements BarcodeCaptu
 
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        pauseFrameSource();
-    }
-
-    @Override
-    protected void onDestroy() {
-        barcodeCapture.removeListener(this);
-        dataCaptureContext.removeMode(barcodeCapture);
-        super.onDestroy();
-    }
-
-    private void pauseFrameSource() {
-        // Switch camera off to stop streaming frames.
-        // The camera is stopped asynchronously and will take some time to completely turn off.
-        // Until it is completely stopped, it is still possible to receive further results, hence
-        // it's a good idea to first disable barcode capture as well.
-        barcodeCapture.setEnabled(false);
-        camera.switchToDesiredState(FrameSourceState.OFF, null);
-    }
 
     private void scanStores(String mobile){
         Map<String,String> params=new HashMap<>();
@@ -544,48 +476,9 @@ public class ScannarActivity extends NetworkBaseActivity implements BarcodeCaptu
 
     @Override
     public void onDialogPositiveClicked(){
-        if(Utility.verifyCameraPermissions(this)){
-            barcodeCapture.setEnabled(true);
-            camera.switchToDesiredState(FrameSourceState.ON, null);
-        }
-    }
-
-
-    @Override
-    public void onBarcodeScanned(BarcodeCapture barcodeCapture, BarcodeCaptureSession barcodeCaptureSession, FrameData frameData) {
-        if (barcodeCaptureSession.getNewlyRecognizedBarcodes().isEmpty()) return;
-
-        // List<Barcode> recognizedBarcodes = barcodeCaptureSession.getNewlyRecognizedBarcodes();
-        final com.scandit.datacapture.barcode.data.Barcode barcode = barcodeCaptureSession.getNewlyRecognizedBarcodes().get(0);
-        final String symbology = SymbologyDescription.create(barcode.getSymbology()).getReadableName();
-        Log.i(TAG,"barcode "+barcode.getData()+ " (" + symbology + ")");
-        qrCode = barcode.getData();
-        barcodeCapture.setEnabled(false);
-
-
-
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                processResult(qrCode);
-                //showMyDialog("barcode "+barcode.getData()+ " (" + symbology + ")");
+            if(Utility.verifyCameraPermissions(this)){
+                resetBarCodeScanner();
             }
-        });
-    }
-
-    @Override
-    public void onObservationStarted(BarcodeCapture barcodeCapture) {
-
-    }
-
-    @Override
-    public void onObservationStopped(BarcodeCapture barcodeCapture) {
-
-    }
-
-    @Override
-    public void onSessionUpdated(BarcodeCapture barcodeCapture, BarcodeCaptureSession barcodeCaptureSession, FrameData frameData) {
-
     }
 
     @Override
@@ -601,6 +494,5 @@ public class ScannarActivity extends NetworkBaseActivity implements BarcodeCaptu
                 }
                 break;
         }
-
     }
 }
